53d3d6fa74efddac376ad4c40d8fecb52a7b5e9a
SuperStack <superstack.pro@outlook.com>
2021-04-13 17:43:20 +0800

    ⚡ 修改展示图片

--- src/components/IndexShow/Photo/index.tsx
+++ src/components/IndexShow/Photo/index.tsx
@@ -5,43 +5,35 @@ import "./index.less";
 const PHOTOS = [
   {
     name: "自然",
-    thumb:
-      "https://images.unsplash.com/photo-1573951875348-cfbd16f5ec0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2168&q=80",
+    thumb: "/photos/nature.jpg",
   },
   {
-    name: "山",
-    thumb:
-      "https://images.unsplash.com/photo-1574001412492-7555e61a9b53?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    name: "雪山",
+    thumb: "/photos/mountain-1.jpg",
   },
   {
     name: "建筑",
-    thumb:
-      "https://images.unsplash.com/photo-1574033168889-cfc0ed3cc35c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+    thumb: "/photos/building.jpg",
   },
   {
-    name: "天空",
-    thumb:
-      "https://images.unsplash.com/photo-1574011435834-e5393d4f2d1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-  },
-  {
-    name: "人",
-    thumb:
-      "https://images.unsplash.com/photo-1573980126659-9a8d74a435c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    name: "街头",
+    thumb: "/photos/city.jpg",
   },
   {
     name: "城市",
-    thumb:
-      "https://images.unsplash.com/photo-1573924475028-1db9e3a475df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+    thumb: "/photos/city-1.jpg",
   },
   {
-    name: "交通",
-    thumb:
-      "https://images.unsplash.com/photo-1573941097299-fd8c30a75b4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    name: "人",
+    thumb: "/photos/people.jpg",
   },
   {
-    name: "泰国",
-    thumb:
-      "https://images.unsplash.com/photo-1573948427350-47fa33d9880c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    name: "海",
+    thumb: "/photos/sea.jpg",
+  },
+  {
+    name: "夜晚",
+    thumb: "/photos/night.jpg",
   },
 ];
 
